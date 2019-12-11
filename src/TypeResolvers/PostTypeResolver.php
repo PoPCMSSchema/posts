828a7b89261e25d4247e65562c3f49863dd70d67
Leo <losoviz@gmail.com>
2019-12-11 10:23:46 +0800

    Renamed getTypeCollectionName to getTypeName

--- src/TypeResolvers/PostTypeResolver.php
+++ src/TypeResolvers/PostTypeResolver.php
@@ -6,11 +6,11 @@ use PoP\Posts\TypeDataResolvers\PostTypeDataResolver;
 
 class PostTypeResolver extends AbstractTypeResolver
 {
-    public const TYPE_COLLECTION_NAME = 'posts';
+    public const NAME = 'posts';
 
-    public function getTypeCollectionName(): string
+    public function getTypeName(): string
     {
-        return self::TYPE_COLLECTION_NAME;
+        return self::NAME;
     }
 
     public function getId($resultItem)
