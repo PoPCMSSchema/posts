8b8b11a4540c7b1570830d43a117c1fa3ee843df
Leo <losoviz@gmail.com>
2019-12-20 15:40:06 +0800

    Automatically validate enum types

--- src/FieldResolvers/PostFieldResolver.php
+++ src/FieldResolvers/PostFieldResolver.php
@@ -33,34 +33,6 @@ class PostFieldResolver extends AbstractDBDataFieldResolver
         ];
     }
 
-    public function resolveSchemaValidationErrorDescription(TypeResolverInterface $typeResolver, string $fieldName, array $fieldArgs = []): ?string
-    {
-        if ($error = parent::resolveSchemaValidationErrorDescription($typeResolver, $fieldName, $fieldArgs)) {
-            return $error;
-        }
-
-        // Important: The validations below can only be done if no fieldArg contains a field!
-        // That is because this is a schema error, so we still don't have the $resultItem against which to resolve the field
-        // For instance, this doesn't work: /?query=arrayItem(posts(),3)
-        // In that case, the validation will be done inside ->resolveValue(), and will be treated as a $dbError, not a $schemaError
-        if (!FieldQueryUtils::isAnyFieldArgumentValueAField($fieldArgs)) {
-            $translationAPI = TranslationAPIFacade::getInstance();
-            switch ($fieldName) {
-                case 'is-status':
-                    $status = $fieldArgs['status'];
-                    if (!in_array($status, PublishableFieldInterfaceResolver::POST_STATUSES)) {
-                        return sprintf(
-                            $translationAPI->__('Argument \'status\' can only have these values: \'%s\'', 'pop-posts'),
-                            implode($translationAPI->__('\', \''), PublishableFieldInterfaceResolver::POST_STATUSES)
-                        );
-                    }
-                    break;
-            }
-        }
-
-        return null;
-    }
-
     public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
     {
         $cmspostsresolver = \PoP\Posts\ObjectPropertyResolverFactory::getInstance();
