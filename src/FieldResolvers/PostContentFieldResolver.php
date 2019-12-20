e7f9fb1ba0356e3519e01ca48d922850617062a0
Leo <losoviz@gmail.com>
2019-12-20 23:27:58 +0800

    Created an abstract ContentEntityFieldResolver

--- src/FieldResolvers/PostContentFieldResolver.php
+++ src/FieldResolvers/PostContentFieldResolver.php
@@ -1,81 +1,21 @@
 <?php
 namespace PoP\Posts\FieldResolvers;
 
-use PoP\Hooks\Facades\HooksAPIFacade;
-use PoP\Posts\TypeResolvers\PostTypeResolver;
-use PoP\LooseContracts\Facades\NameResolverFacade;
-use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
-use PoP\Content\FieldInterfaces\ContentFieldInterfaceResolver;
-use PoP\Content\FieldInterfaces\LinkableFieldInterfaceResolver;
-use PoP\Content\FieldInterfaces\PublishableFieldInterfaceResolver;
-use PoP\ComponentModel\FieldResolvers\AbstractDBDataFieldResolver;
 use PoP\Posts\Facades\PostTypeAPIFacade;
+use PoP\Posts\TypeResolvers\PostTypeResolver;
+use PoP\Content\TypeAPIs\ContentEntityTypeAPIInterface;
+use PoP\Content\FieldResolvers\AbstractContentEntityFieldResolver;
 
-class PostContentFieldResolver extends AbstractDBDataFieldResolver
+class PostContentFieldResolver extends AbstractContentEntityFieldResolver
 {
     public static function getClassesToAttachTo(): array
     {
         return array(PostTypeResolver::class);
     }
 
-    public static function getFieldNamesToResolve(): array
+    protected function getContentEntityTypeAPI(): ContentEntityTypeAPIInterface
     {
-        return [];
-    }
-
-    public static function getImplementedInterfaceClasses(): array
-    {
-        return [
-            ContentFieldInterfaceResolver::class,
-            LinkableFieldInterfaceResolver::class,
-            PublishableFieldInterfaceResolver::class,
-        ];
-    }
-
-    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
-    {
-        $cmspostsresolver = \PoP\Posts\ObjectPropertyResolverFactory::getInstance();
-        $cmsengineapi = \PoP\Engine\FunctionAPIFactory::getInstance();
         $postTypeAPI = PostTypeAPIFacade::getInstance();
-        $post = $resultItem;
-        switch ($fieldName) {
-            case 'post-type':
-                return $cmspostsresolver->getPostType($post);
-
-            case 'title':
-                return $postTypeAPI->getTitle($post);
-
-            case 'content':
-                $value = $postTypeAPI->getContent($post);
-                return HooksAPIFacade::getInstance()->applyFilters('pop_content', $value, $typeResolver->getID($post));
-
-            case 'url':
-                return $postTypeAPI->getPermalink($post);
-
-            case 'excerpt':
-                return $postTypeAPI->getExcerpt($post);
-
-            case 'status':
-                return $postTypeAPI->getStatus($post);
-
-            case 'is-status':
-                return $fieldArgs['status'] == $postTypeAPI->getStatus($post);
-
-            case 'date':
-                $format = $fieldArgs['format'] ?? $cmsengineapi->getOption(NameResolverFacade::getInstance()->getName('popcms:option:dateFormat'));
-                return $cmsengineapi->getDate($format, $postTypeAPI->getPublishedDate($post));
-
-            case 'datetime':
-                // If it is the current year, don't add the year. Otherwise, do
-                // 15 Jul, 21:47 or // 15 Jul 2018, 21:47
-                $date = $postTypeAPI->getPublishedDate($post);
-                $format = $fieldArgs['format'];
-                if (!$format) {
-                    $format = ($cmsengineapi->getDate('Y', $date) == date('Y')) ? 'j M, H:i' : 'j M Y, H:i';
-                }
-                return $cmsengineapi->getDate($format, $date);
-        }
-
-        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+        return $postTypeAPI;
     }
 }
---
+++ src/FieldResolvers/PostPostTypeFieldResolver.php
@@ -0,0 +1,52 @@
+<?php
+namespace PoP\Posts\FieldResolvers;
+
+use PoP\Posts\TypeResolvers\PostTypeResolver;
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\ComponentModel\FieldResolvers\AbstractDBDataFieldResolver;
+
+class PostPostTypeFieldResolver extends AbstractDBDataFieldResolver
+{
+    public static function getClassesToAttachTo(): array
+    {
+        return array(PostTypeResolver::class);
+    }
+
+    public static function getFieldNamesToResolve(): array
+    {
+        return [
+            'post-type',
+        ];
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $types = [
+            'post-type' => SchemaDefinition::TYPE_STRING,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        $descriptions = [
+            'post-type' => $translationAPI->__('Post type', 'content'),
+        ];
+        return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
+    {
+        $cmspostsresolver = \PoP\Posts\ObjectPropertyResolverFactory::getInstance();
+        $post = $resultItem;
+        switch ($fieldName) {
+            case 'post-type':
+                return $cmspostsresolver->getPostType($post);
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+}
