c950667f84837b17cd01b453462027a28c81bd59
Leo <losoviz@gmail.com>
2020-03-17 01:38:50 +0800

    Added TypeRegistry

--- src/Component.php
+++ src/Component.php
@@ -37,6 +37,7 @@ class Component extends AbstractComponent
         parent::boot();
 
         // Initialize classes
+        ContainerBuilderUtils::registerTypeResolversFromNamespace(__NAMESPACE__.'\\TypeResolvers');
         ContainerBuilderUtils::attachFieldResolversFromNamespace(__NAMESPACE__.'\\FieldResolvers');
         self::attachTypeResolverPickers();
     }
