7083b1fca84272a92d7add19bc5b70d4f863c220
Leo <losoviz@gmail.com>
2020-04-11 19:35:02 +0800

    Removed PostsAPI package, moved code here

---
+++ src/FieldResolvers/RootPostFieldResolver.php
@@ -0,0 +1,99 @@
+<?php
+namespace PoP\Posts\FieldResolvers;
+
+use PoP\Engine\TypeResolvers\RootTypeResolver;
+use PoP\Posts\FieldResolvers\AbstractPostFieldResolver;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\Posts\TypeResolvers\PostTypeResolver;
+use PoP\Posts\Facades\PostTypeAPIFacade;
+
+class RootPostFieldResolver extends AbstractPostFieldResolver
+{
+    public static function getClassesToAttachTo(): array
+    {
+        return array(RootTypeResolver::class);
+    }
+
+    public static function getFieldNamesToResolve(): array
+    {
+        return array_merge(
+            parent::getFieldNamesToResolve(),
+            [
+                'post',
+            ]
+        );
+    }
+
+    public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        $descriptions = [
+			'post' => $translationAPI->__('Post with a specific ID', 'posts'),
+        ];
+        return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $types = [
+			'post' => SchemaDefinition::TYPE_ID,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): array
+    {
+        $schemaFieldArgs = parent::getSchemaFieldArgs($typeResolver, $fieldName);
+        $translationAPI = TranslationAPIFacade::getInstance();
+        switch ($fieldName) {
+            case 'post':
+                return array_merge(
+                    $schemaFieldArgs,
+                    [
+                        [
+                            SchemaDefinition::ARGNAME_NAME => 'id',
+                            SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_ID,
+                            SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The post ID', 'pop-posts'),
+                            SchemaDefinition::ARGNAME_MANDATORY => true,
+                        ],
+                    ]
+                );
+        }
+        return $schemaFieldArgs;
+    }
+
+    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
+    {
+        $postTypeAPI = PostTypeAPIFacade::getInstance();
+        switch ($fieldName) {
+            case 'post':
+                $query = [
+                    'include' => [$fieldArgs['id']],
+                    'post-status' => [
+                        POP_POSTSTATUS_PUBLISHED,
+                    ],
+                ];
+                $options = [
+                    'return-type' => POP_RETURNTYPE_IDS,
+                ];
+                if ($posts = $postTypeAPI->getPosts($query, $options)) {
+                    return $posts[0];
+                }
+                return null;
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+
+    public function resolveFieldTypeResolverClass(TypeResolverInterface $typeResolver, string $fieldName, array $fieldArgs = []): ?string
+    {
+        switch ($fieldName) {
+            case 'post':
+                return PostTypeResolver::class;
+        }
+
+        return parent::resolveFieldTypeResolverClass($typeResolver, $fieldName, $fieldArgs);
+    }
+}
