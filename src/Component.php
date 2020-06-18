1f30b663c2aeb509f722fb00be96074c59e1be53
Leonardo Losoviz <leo@getpop.org>
2020-06-18 08:59:23 +0800

    Fixed dependency on QueriedObject

--- src/Component.php
+++ src/Component.php
@@ -25,7 +25,6 @@ class Component extends AbstractComponent
     public static function getDependedComponentClasses(): array
     {
         return [
-            \PoP\QueriedObject\Component::class,
             \PoP\CustomPosts\Component::class,
         ];
     }
