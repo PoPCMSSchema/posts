a1efb95c5cf970f98e9ba2a902955cc1ecc77925
Leonardo Losoviz <leo@getpop.org>
2020-10-14 13:24:41 +0800

    Use autowiring

--- src/Config/ServiceConfiguration.php
+++ src/Config/ServiceConfiguration.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace PoPSchema\Posts\Config;
 
-use PoP\ComponentModel\Container\ContainerBuilderUtils;
 use PoP\Root\Component\PHPServiceConfigurationTrait;
+use PoP\ComponentModel\Container\ContainerBuilderUtils;
+use PoP\ModuleRouting\RouteModuleProcessorManagerInterface;
 
 class ServiceConfiguration
 {
@@ -17,14 +18,14 @@ class ServiceConfiguration
         // Load API and RESTAPI conditional classes
         if (class_exists('\PoP\API\Component') && \PoP\API\Component::isEnabled()) {
             ContainerBuilderUtils::injectServicesIntoService(
-                'route_module_processor_manager',
+                RouteModuleProcessorManagerInterface::class,
                 'PoPSchema\\Posts\\Conditional\\API\\RouteModuleProcessors',
                 'add'
             );
         }
         if (class_exists('\PoP\RESTAPI\Component') && \PoP\RESTAPI\Component::isEnabled()) {
             ContainerBuilderUtils::injectServicesIntoService(
-                'route_module_processor_manager',
+                RouteModuleProcessorManagerInterface::class,
                 'PoPSchema\\Posts\\Conditional\\RESTAPI\\RouteModuleProcessors',
                 'add'
             );
@@ -35,14 +36,14 @@ class ServiceConfiguration
             // Load API and RESTAPI conditional classes
             if (class_exists('\PoP\API\Component') && \PoP\API\Component::isEnabled()) {
                 ContainerBuilderUtils::injectServicesIntoService(
-                    'route_module_processor_manager',
+                    RouteModuleProcessorManagerInterface::class,
                     'PoPSchema\\Posts\\Conditional\\Users\\Conditional\\API\\RouteModuleProcessors',
                     'add'
                 );
             }
             if (class_exists('\PoP\RESTAPI\Component') && \PoP\RESTAPI\Component::isEnabled()) {
                 ContainerBuilderUtils::injectServicesIntoService(
-                    'route_module_processor_manager',
+                    RouteModuleProcessorManagerInterface::class,
                     'PoPSchema\\Posts\\Conditional\\Users\\Conditional\\RESTAPI\\RouteModuleProcessors',
                     'add'
                 );
@@ -53,14 +54,14 @@ class ServiceConfiguration
             // Load API and RESTAPI conditional classes
             if (class_exists('\PoP\API\Component') && \PoP\API\Component::isEnabled()) {
                 ContainerBuilderUtils::injectServicesIntoService(
-                    'route_module_processor_manager',
+                    RouteModuleProcessorManagerInterface::class,
                     'PoPSchema\\Posts\\Conditional\\Tags\\Conditional\\API\\RouteModuleProcessors',
                     'add'
                 );
             }
             if (class_exists('\PoP\RESTAPI\Component') && \PoP\RESTAPI\Component::isEnabled()) {
                 ContainerBuilderUtils::injectServicesIntoService(
-                    'route_module_processor_manager',
+                    RouteModuleProcessorManagerInterface::class,
                     'PoPSchema\\Posts\\Conditional\\Tags\\Conditional\\RESTAPI\\RouteModuleProcessors',
                     'add'
                 );
